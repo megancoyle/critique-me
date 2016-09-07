96013c4a16b20c2426239038f4005cdeb4a84db0
Megan Coyle <meg@mcoyle.com>
2016-09-06 20:50:24 -0400

    Update connection

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@
 var mongoose = require("mongoose");
 
 if(process.env.NODE_ENV == "production"){
-  mongoose.connect(process.env.MONGOLAB_URI);
+  mongoose.connect(process.env.heroku_65cws70w);
 }else{
 mongoose.connect("mongodb://localhost/critique-me");
 }
